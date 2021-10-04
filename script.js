9583459f7941f75cdb19fe9601125654f504d342
RenanMoreirarjt <91581623+RenanMoreirarjt@users.noreply.github.com>
2021-10-04 00:11:48 -0300

    Adicionei os arquivos necessários do Certificard

---
+++ script.js
@@ -0,0 +1,18 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+  
+  
+}
+
+
+
+// para se usar uma classe no css se designa com ."nome da classe" estilo .container
+// o padding da uma margem entre os elementos internos do objeto que esta se estilizando no css para o próprio objeto
+
+// os elementos filhos do header são a imagem e a div, por isso ao alinhar ao centro os dois textos dentro de h1 e h3 ficaram um em cima do outro ao invés de todos centralizados
+
+// cada h representa um tamanho menor de título
+
+// fazer uma estilização de algo do zero como desafio
+
+// no padding o primeiro numero são as margens de top e fundo e o segundo para margens de direita e esquerda
